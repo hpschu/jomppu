450909b082f0a1a37c53167338f9462254b6494a
hpschu <hpschukov@gmail.com>
2023-12-24 16:08:51 +0200

    fixed problem with stuck ball

--- harderField.js
+++ harderField.js
@@ -8,7 +8,7 @@ class HarderField extends PlayField {
         this.endCup = new EndCup(250, 500);
         this.ball = new Ball(this.startPosX, this.startPosY, this.ballRadius, this.ballOptions);
         var bar = Bodies.rectangle(200, 400, 10, 120, this.barrierOptions);
-        var block = Bodies.polygon(324, 423, 6, 8, this.barrierOptions);
+        var block = Bodies.polygon(340, 423, 6, 8, this.barrierOptions);
         Matter.Body.rotate(bar, 45);
         Matter.Composite.add(this.engine.world, [
             this.endCup.body,
